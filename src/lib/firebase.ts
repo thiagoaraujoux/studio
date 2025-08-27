24309a14d2d363430584d7160f0fd4b4f94c22a0
Thiago Araújo <thiagoaraujo.tec@gmail.com>
2025-08-27 13:16:14 +0000

    const firebaseConfig = {   apiKey: "AIzaSyBKMQyszHdwTrCC0v4YXCFisSY9CcaU

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,3 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -7,13 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "vitalize-companion",
-  "appId": "1:1055635334907:web:e891cf3c8cd98cedc9725e",
-  "storageBucket": "vitalize-companion.firebasestorage.app",
-  "apiKey": "AIzaSyBKMQyszHdwTrCC0v4YXCFisSY9CcaU5Xg",
-  "authDomain": "vitalize-companion.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1055635334907"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
