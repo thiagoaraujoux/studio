8e75b4ccaf111fb24f2af3b87b606ad1cfbcb663
Thiago Araújo <thiagoaraujo.tec@gmail.com>
2025-08-27 13:52:30 +0000

    se o backend esta esperando monospace-15 onde eu troco esse configuração

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -12,9 +12,13 @@ if (!admin.apps.length) {
     admin.initializeApp({
       projectId: projectId,
     });
+    console.log("Firebase Admin SDK inicializado para o projeto:", projectId);
   } catch (error) {
     console.error('Firebase admin initialization error', error);
   }
+} else {
+  console.log("Firebase Admin SDK já inicializado.");
 }
+
 
 export const authAdmin = admin.auth();
