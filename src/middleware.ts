215a3d287287f0c1db9ede066cdfaf1ee02101ac
Thiago Araújo <thiagoaraujo.tec@gmail.com>
2025-08-27 13:37:54 +0000

    desative a tela de login por enquanto

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,17 +3,18 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
-  const session = request.cookies.get('session');
+  // A verificação de sessão foi desativada temporariamente.
+  // const session = request.cookies.get('session');
 
-  // Se não houver sessão e o usuário tentar acessar qualquer página exceto /login, redirecione para /login
-  if (!session && request.nextUrl.pathname !== '/login') {
-    return NextResponse.redirect(new URL('/login', request.url));
-  }
+  // // Se não houver sessão e o usuário tentar acessar qualquer página exceto /login, redirecione para /login
+  // if (!session && request.nextUrl.pathname !== '/login') {
+  //   return NextResponse.redirect(new URL('/login', request.url));
+  // }
 
-  // Se houver uma sessão e o usuário tentar acessar /login, redirecione para a página inicial
-  if (session && request.nextUrl.pathname === '/login') {
-     return NextResponse.redirect(new URL('/', request.url));
-  }
+  // // Se houver uma sessão e o usuário tentar acessar /login, redirecione para a página inicial
+  // if (session && request.nextUrl.pathname === '/login') {
+  //    return NextResponse.redirect(new URL('/', request.url));
+  // }
 
   return NextResponse.next();
 }
